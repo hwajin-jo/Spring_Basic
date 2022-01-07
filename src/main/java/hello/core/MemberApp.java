19c874a91d443c3ffd8b3a993e2bbe8ce0b144cf
hwajin-jo <wa8784@naver.com>
2022-01-07 18:38:41 +0900

    22/01/07

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,18 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService(); // appconfig에서 결정된 구현체를 적용
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService(); // appconfig에서 결정된 구현체를 적용
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
 //        MemberService memberService = new MemberServiceImpl();
         Member member = new Member(1L, "memberA", Grade.BASIC);
         memberService.join(member);
--- src/main/java/hello/core/member/Member.java
+++ src/main/java/hello/core/member/Member.java
@@ -20,18 +20,22 @@ public class Member {
     }
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public Grade getGrade() {
+
         return grade;
     }
 
     public void setGrade(Grade grade) {
+
         this.grade = grade;
     }
 }
